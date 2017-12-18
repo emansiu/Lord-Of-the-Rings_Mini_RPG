1467827d4123c2c8b834149e4eb72f2a1a494c74
Emanuel Siu <e@emanuelsiu.com>
2017-12-17 22:29:03 -0500

    some functionality in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+// attack power = ap
+// counter attack = ca
+// health points = hp
+
+var characters = {
+    gandalf: {
+        hp: 150,
+        ca:20,
+        ap:10
+    },
+    legolas: {
+        hp: 120,
+        ca:20,
+        ap:8
+    },
+    frodo: {
+        hp: 100,
+        ca:5,
+        ap:11
+    },
+    sauron: {
+        hp: 180,
+        ca:20,
+        ap:15
+    }
+    
+}
+
+
+$("#current-enemy").on("click", function(){
+    $("#current-enemy").text(characters.sauron.ap);
+});
+
+});
